25d934332c4e4be603ac62462ea6ea74c199d112
Amanda Henneberry <amandahenneberry@icloud.com>
2022-01-22 17:22:43 -0500

    'added button for react fact itteration'

--- client/components/GuestHome.js
+++ client/components/GuestHome.js
@@ -4,24 +4,42 @@ import {connect} from 'react-redux'
 /**
  * COMPONENT
  */
+
+
 class GuestHome extends React.Component{
-//   constructor(){
-//     super()
-//     this.state = {
-//       fact: ['fact 1', 'fact 2', 'fact 3'],
-//       currentFact: 'click for \nCAW A\n facts'
-//     }
-//     this.handleClick = this.handleClick.bind(this)
-//   }
-//   componentDidMount() {
-//     // document.addEventListener("click", this.handleClick) -> gives an answer whenever the user clicks
-//     document.addEventListener("keydown", this.handleKeyDown)
-// }
+  constructor(){
+    super()
+    this.state = {
+      facts: ['fact 1', 'fact 2', 'fact 3', 'fact 4', 'fact 5', 'fact 6', 'fact 7', 'fact 8', 'fact 9', 'fact 10'],
+      currentFact: 'click for \nCAW A\n facts',
+      sound: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    // const index = Math.floor(Math.random()* this.state.facts.length);
+    // this.setState({currentFact: this.state.facts[index]})
+
+    // const index = Math.floor(Math.random()* this.state.facts.length);
+    // this.setState({currentFact: this.state.facts.filter(function(fact){
+    //   return fact !== [index]
+    // })})
+
+    const index = Math.floor(Math.random()* this.state.facts.length);
+    this.setState.sound = true;
+    this.setState({currentFact: this.state.facts[index]});
+  }
 
   render(){
     return (
       <div>
         <h3>Welcome CAW CcccAW</h3>
+        <div>
+          <h2>{this.state.currentFact}</h2>
+          <button type="button" onClick={() => this.handleClick()}>CLICK ME</button>
+
+        </div>
       </div>
     )
   }
